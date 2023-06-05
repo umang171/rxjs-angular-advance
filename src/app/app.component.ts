13e954d25fc6abb5a3823045e12ebeb946061372
Umang Gohel <gohelumang12@gmail.com>
2023-06-05 17:14:07 +0530

    subject example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Subject, interval, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'rxjs-advance';
+  
+  observer={
+    next:(val:any)=>console.log('next-',val),
+    error:(err:any)=>console.error(err),
+    complte:console.log("Completed")    
+  }
+  subject=new Subject();
+  subscriber1=this.subject.subscribe(this.observer);
+  subscriber2=this.subject.subscribe(this.observer);
+
+  intervalObs=interval(1000).pipe(tap(val=>console.log("New value",val)));
+  constructor(){
+    this.subject.next("Hello");
+    this.intervalObs.subscribe(this.subject);
+  }
 }
