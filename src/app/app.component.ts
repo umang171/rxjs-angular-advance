c6b9023236d0d2f6778e8f366f31450d53efd7ec
Umang Gohel <gohelumang12@gmail.com>
2023-06-05 17:26:29 +0530

    loading state with subject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Subject, interval, tap } from 'rxjs';
+import { Observable, Subject, interval, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -14,12 +14,19 @@ export class AppComponent {
     complte:console.log("Completed")    
   }
   subject=new Subject();
+  subject2=new Subject();
   subscriber1=this.subject.subscribe(this.observer);
   subscriber2=this.subject.subscribe(this.observer);
 
   intervalObs=interval(1000).pipe(tap(val=>console.log("New value",val)));
   constructor(){
-    this.subject.next("Hello");
-    this.intervalObs.subscribe(this.subject);
+    // this.subject.next("Hello");
+    // this.intervalObs.subscribe(this.subject);
+    this.subject2.subscribe(isLogin=>isLogin?console.log("successfull login"):console.log("Logout"));
+    this.subject2.next(true);
+    setTimeout(() => {
+    this.subject2.next(false);
+      
+    }, 3000);
   }
 }
